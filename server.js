c7bfd605b92a1410ef7402ffb8617844b2a2d63e
Christopher Maples <chris.maples@logicnow.com>
2015-07-01 06:14:13 +0100

    npm start, file operations, security

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+#!/usr/bin/env node
+
 var http = require('http');
 var postdata = require('post-data');
 var fs = require('fs');
 var Path = require('path');
 var mkdirp = require('mkdirp');
+var rmdir = require('rmdir');
 
 var config = {
     port: 6504, //JSON
@@ -21,28 +24,101 @@ function uhoh(err, res, code) {
     }
 }
 
-function fwrite(data, fileName, callback) {
-    fs.writeFile(fileName, JSON.stringify(data, null, 4), callback || function(err) {
-        console.error(err);
-    });
-}
-
-function fread(path, callback) {
-    fs.readFile(path, {
-        encoding: 'utf8'
-    }, callback || function (err, data) {
-      if (err) {
-          console.error(err);
-      }
-      console.log(data);
-    });
-}
-
 function ok(data, res) {
     res.writeHead(200, {
         'Content-Type': config.contentType
     });
     res.end(JSON.stringify(data));
+}
+
+function canFile(path) {
+    console.log(Path.resolve(path), Path.resolve(config.fileBase));
+    return Path.resolve(path).indexOf(Path.resolve(config.fileBase)) > -1 /*&& Path.dirname(path) !== Path.resolve(config.fileBase)*/;
+}
+
+function fwrite(data, fileName, callback) {
+    var willCreate = fileName && canFile(fileName);
+    if (willCreate) {
+        fs.writeFile(fileName, JSON.stringify(data, null, 4), callback || function (err) {
+            if (err) {
+                throw err;
+            }
+        });
+    } else {
+        callback(new Error('Oops'));
+    }
+
+}
+
+function fread(path, callback) {
+    var willRead = path && canFile(path);
+    if (willRead) {
+        fs.readFile(path, {
+            encoding: 'utf8'
+        }, callback || function (err, data) {
+            if (err) {
+                throw err;
+            }
+            console.log(data);
+        });
+    } else {
+        callback(new Error('Oops'));
+    }
+}
+
+function fdelete(path, callback) {
+    var willDelete = path && canFile(path);
+    if (willDelete) {
+        fs.unlink(path, callback || function (err) {
+            if (err) {
+                throw err;
+            } else {
+                console.log(path, 'deleted');
+            }
+        });
+    }
+}
+
+function ddelete(path, callback) {
+    var willDelete = path && canFile(path);
+    if (willDelete) {
+        rmdir(path, callback || function () {
+            console.log(path, 'deleted');
+        });
+        // fs.rmdir(path, callback || function() {
+        //     console.log(path, 'deleted');
+        // });
+    } else {
+        callback();
+    }
+}
+
+function fexists(path, callback) {
+    callback = callback || function (exists) {};
+    fs.stat(path, function (err, stat) {
+        var exists = false;
+        if (err || !stat) {
+            exists = false;
+        } else {
+            exists = stat.isFile();
+        }
+        callback(exists);
+        console.log(path, exists ? 'exists' : "doesn't exist");
+    });
+}
+
+function dexists(path, callback) {
+    callback = callback || function (exists) {};
+    fs.stat(path, function (err, stat) {
+        var exists = false;
+        if (err || !stat) {
+            exists = false;
+        } else {
+            exists = stat.isDirectory();
+        }
+        callback(exists);
+        console.log(path, exists ? 'exists' : "doesn't exist");
+    });
 }
 
 var middleWare = [
@@ -52,11 +128,23 @@ var middleWare = [
     },
 
     function (req, res) {
-        res.ok = function(data) {
+        //CORS enabled
+        if (!res.headersSent) {
+            res.setHeader("Access-Control-Allow-Origin", "*");
+            res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
+        }
+    },
+
+    function (req, res) {
+        res.ok = function (data) {
             ok(data, res);
         };
-        res.uhoh = function(err, code) {
-            uhoh(err, res, code || err.number || err.code);
+        res.uhoh = function (err, code) {
+            var status = code;
+            if (err) {
+                status = code || err.number || err.code;
+            }
+            uhoh(err, res, status);
         };
     },
 
@@ -67,37 +155,77 @@ var middleWare = [
         var fileName = realPath + ".json";
 
         switch (req.method.toLowerCase()) {
-            case 'post':
-            case 'put':
-            case 'patch':
-                postdata(req, function (error, data) {
-                    res.uhoh(error);
-                    mkdirp(parentPath, function (err) {
+        case 'post':
+        case 'put':
+        case 'patch':
+            postdata(req, function (error, data) {
+                res.uhoh(error);
+                mkdirp(parentPath, function (err) {
+                    res.uhoh(err);
+                    fwrite(data, fileName, function (err) {
                         res.uhoh(err);
-                        fwrite(data, fileName, function(err) {
-                            res.uhoh(err);
-                            res.ok(data);
-                        });
+                        res.ok(data);
                     });
                 });
-                break;
-            case 'delete':
-                res.uhoh('Not Implemented', 501);
-                break;
-            default:
-                fread(fileName, function(err, data) {
-                    res.uhoh(err);
-                    res.ok(JSON.parse(data));
-                });
+            });
+            break;
+        case 'delete':
+            // res.uhoh('Not Implemented', 501);
+
+            fexists(fileName, function (exists) {
+                if (exists) {
+                    fdelete(fileName, function (err) {
+                        res.uhoh(err);
+                        res.ok({
+                            filename: fileName,
+                            status: 'Deleted',
+                            timestamp: new Date().getTime()
+                        });
+                    });
+                } else {
+                    dexists(realPath, function (exists) {
+                        if (exists) {
+                            ddelete(realPath, function () {
+                                res.ok({
+                                    filename: realPath,
+                                    status: 'Deleted',
+                                    timestamp: new Date().getTime()
+                                });
+                            });
+                        } else {
+                            res.uhoh('Not Found', 404);
+                        }
+                    });
+                }
+            });
+
+
+            break;
+        default:
+            fexists(fileName, function (exists) {
+                if (exists) {
+                    fread(fileName, function (err, data) {
+                        res.uhoh(err);
+                        res.ok(JSON.parse(data));
+                    });
+                } else {
+                    res.uhoh('Not Found', 404);
+                }
+            });
         }
     }
 
 ];
 
 http.createServer(function (req, res) {
-    for(var i = 0; i < middleWare.length; ++i) {
-        middleWare[i](req, res);
+    try {
+        for (var i = 0; i < middleWare.length; ++i) {
+            middleWare[i](req, res);
+        }
+    } catch (ex) {
+        res.uhoh(ex);
     }
+
 }).listen(config.port, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:' + config.port + '/');
